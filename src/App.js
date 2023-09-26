ca89e885a88c1b4e06d61f27e4309f1de62a216f
raghadtaha19 <raghad.taha198@gmail.com>
2023-09-26 12:12:14 +0300

    raghad

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import  './to_do_add_delete.css';
+import TodoList from './to_do_add_delete';
+
+
+function App() {
+  
+    return (
+      <div>
+      <TodoList/>
+      </div>
+     
+    );
+  }
+  // In the component's return statement, we define the JSX (JavaScript XML) that will be rendered on the screen
+  
+  export default App;
+  
